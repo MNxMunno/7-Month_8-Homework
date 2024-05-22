782032b64920b8d8a18e779fc6f6bd7bdc9930c2
MNxMunno <bilolhuzayfa@gmail.com>
2024-05-23 02:50:26 +0500

    channge

--- src/routers/home/Home.jsx
+++ src/routers/home/Home.jsx
@@ -6,7 +6,7 @@ import { useFetch } from "../../hooks/useFetch";
 const Home = () => {
   return (
     <div className="container">
-      <h1>Home</h1>
+      {/* <h1>Home</h1> */}
       <Product />
     </div>
   );
